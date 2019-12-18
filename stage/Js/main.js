9608d4db15a145dd7df82c63c446b81a7ee62805
mohamed-Gamal-Nor <midogamal3339@gmail.com>
2019-12-18 20:21:38 +0200

    Front End 18

--- stage/Js/main.js
+++ stage/Js/main.js
@@ -9,4 +9,40 @@ $(function() {
         $(this).find(".fa-angle-right").toggleClass("down");
         $(this).next('.child-links').slideToggle();
     });
+
+    //open /close full screen
+    $(".toggle-fullscreen").on("click", function() {
+        $(this).toggleClass('full-screen');
+        if ($(this).hasClass("full-screen")) { //page is now full screen
+            openFullScreen();
+        } else { //page is not full screen
+            closeFullscreen();
+        }
+    });
+
+    var elem = document.documentElement;
+
+    function openFullScreen() {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.mozRequestFullScreen) { /* Fire Fox*/
+            elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullScreen) { /* Chrome Safari opera*/
+            elem.webkitRequestFullScreen();
+        } else if (elem.msRequestFullScreen) { /* ie/edge*/
+            elem.msRequestFullScreen();
+        }
+    }
+
+    function closeFullscreen() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullscreen) { /* Fire Fox*/
+            document.mozCancelFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Chrome Safari opera*/
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* ie/edge*/
+            document.msExitFullscreen();
+        }
+    }
 });
